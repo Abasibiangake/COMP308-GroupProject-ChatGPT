f228b6aab8fc913582b7570a9548b0b8f99b9958
Abasibiangake James <jamesabasibiangake@gmail.com>
2023-04-19 21:26:19 -0400

    update deploy script

---
+++ groupreact-chatgpt/deploy.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const path = require('path');
+const { WebApi } = require('azure-devops-node-api');
+const { PersonalAccessTokenCredential } = require('azure-devops-node-api/PersonalAccessTokenCredential');
+
+const organizationUrl = 'https://dev.azure.com/ajames87';
+const projectName = 'Group-chatgpt';
+const personalAccessToken = 'twvwgdtlrgmlpuo6xkfudjxct3k4vg3b6wu6jjueazlte3ogobmq';
+const artifactName = 'group-react-artifact';
+const artifactPath = path.join(__dirname, 'build', `${artifactName}.zip`);
+
+const run = async () => {
+  try {
+    // Get a connection to the Azure DevOps organization
+    const credentialHandler = new PersonalAccessTokenCredential(personalAccessToken);
+    const connection = new WebApi(organizationUrl, credentialHandler);
+
+    // Get a reference to the Release API
+    const releaseApi = await connection.getReleaseApi();
+
+    // Read the artifact ZIP file from disk
+    const artifactStream = fs.createReadStream(artifactPath);
+
+    // Upload the artifact to Azure DevOps
+    const uploadedArtifact = await releaseApi.createArtifact(
+      artifactName,
+      projectName,
+      artifactStream
+    );
+
+    // Create a new release
+    const release = {
+      definitionId: 'group-react-releaseApi',
+      description: 'New release created by Azure DevOps pipeline',
+      artifacts: [
+        {
+          alias: artifactName,
+          instanceReference: {
+            id: uploadedArtifact.id.toString(),
+          },
+        },
+      ],
+    };
+    const createdRelease = await releaseApi.createRelease(release, projectName);
+
+    console.log('Release created successfully', createdRelease);
+  } catch (err) {
+    console.error('Failed to create release', err);
+    process.exit(1);
+  }
+};
+
+run();
