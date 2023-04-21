249f77bdf00e7226849f1f7b901688c1c5aacb43
The-indigo <adepojuadeyemi11@gmail.com>
2023-04-20 21:09:41 -0400

    token issue

--- groupreact-chatgpt/src/deploy.js
+++ groupreact-chatgpt/src/deploy.js
@@ -7,7 +7,7 @@ const tl = require('azure-pipelines-task-lib/task');
 const organizationUrl = 'https://dev.azure.com/ajames87';
 const projectName = 'Group-chatgpt';
 const token = tl.getVariable('TOKEN');
-onst artifactName = 'group-react-artifact';
+const artifactName = 'group-react-artifact';
 const buildArtifactFolder = 'build'; // Update this to match your artifact folder name
 const artifactPath = '/home/vsts/work/1/s/groupreact-chatgpt/build/group-react-artifact.zip';
 console.log('token is coming from', token || process.env.TOKEN);
