a09f5117c2c15d51212b0efe35771f600b8b6744
The-indigo <adepojuadeyemi11@gmail.com>
2023-04-20 22:07:35 -0400

    token issue

--- groupreact-chatgpt/src/deploy.js
+++ groupreact-chatgpt/src/deploy.js
@@ -1,6 +1,4 @@
-
 const fs = require('fs');
-// const path = require('path');
 import * as azdev from "azure-devops-node-api";
 import * as ba from "azure-devops-node-api/BuildApi";
 const tl = require('azure-pipelines-task-lib/task');
@@ -29,12 +27,12 @@ const run = async () => {
     // Read the artifact ZIP file from disk
     const artifactStream = fs.createReadStream(artifactPath);
 
-    // Publish the artifact during the build process
-    await buildApi.uploadArtifact(
-      artifactName,
-      artifactStream,
-      fs.statSync(artifactPath).size,
+    // Create a new artifact
+    await build.createArtifact(
       projectName,
+      artifactName,
+      artifactPath,
+      fs.statSync(artifactPath).size,
       'build'
     );
 
@@ -64,59 +62,3 @@ const run = async () => {
 };
 
 run();
-
-// const fs = require('fs');
-// const path = require('path');
-// const  azdev  = require('azure-devops-node-api');
-// const tl = require('azure-pipelines-task-lib/task');
-
-// const organizationUrl = 'https://dev.azure.com/ajames87';
-// const projectName = 'Group-chatgpt';
-// const token = tl.getVariable('TOKEN');
-// const artifactName = 'group-react-artifact';
-// const artifactPath = path.join(__dirname, 'build', `${artifactName}.zip`);
-// console.log('token iscoming  from',token || process.env.TOKEN);
-// const run = async () => {
-//   try {
-//     // Get a connection to the Azure DevOps organization
-//     const credentialHandler = azdev.getPersonalAccessTokenHandler(token || process.env.TOKEN);
-//     const connection = new azdev.WebApi(organizationUrl, credentialHandler);
-
-//     // Get a reference to the Release API
-//     const releaseApi = await connection.getReleaseApi();
-
-//     // Read the artifact ZIP file from disk
-//     const artifactStream = fs.createReadStream(artifactPath);
-
-//     // Upload the artifact to Azure DevOps
-//     const uploadedArtifact = await releaseApi.createArtifactAsync(
-//       artifactName,
-//       projectName,
-//       artifactStream
-//     );
-
-//     // Create a new release
-//     const release = {
-//       definitionId: 'group-react-releaseApi',
-//       description: 'New release created by Azure DevOps pipeline',
-//       artifacts: [
-//         {
-//           alias: artifactName,
-//           instanceReference: {
-//             id: uploadedArtifact.id.toString(),
-//           },
-//         },
-//       ],
-//     };
-//     const createdRelease = await releaseApi.createRelease(release, projectName);
-
-//     console.log('Release created successfully', createdRelease);
-//   } catch (err) {
-//     console.error('Failed to create release', err);
-//     process.exit(1);
-//   }
-// };
-
-// run();
-
-
