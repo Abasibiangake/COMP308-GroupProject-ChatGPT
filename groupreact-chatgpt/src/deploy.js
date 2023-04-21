2b3cea580c63385f85f27a41f35c80a8986c41c3
The-indigo <adepojuadeyemi11@gmail.com>
2023-04-20 23:39:45 -0400

    dev stage yaml

--- groupreact-chatgpt/src/deploy.js
+++ groupreact-chatgpt/src/deploy.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 const azdev = require("azure-devops-node-api");
 const tl = require('azure-pipelines-task-lib/task');
+
 const organizationUrl = 'https://dev.azure.com/ajames87';
 const projectName = 'Group-chatgpt';
 const token = tl.getVariable('TOKEN');
 const artifactName = 'group-react-chatgpt';
 const artifactPath = tl.getVariable('Build.ArtifactStagingDirectory') + '/group-react-chatgpt.zip';
+const releaseDefinitionName = 'release1'; // Specify the release definition name here
 
 const run = async () => {
   try {
@@ -15,8 +17,21 @@ const run = async () => {
     );
     const connection = new azdev.WebApi(organizationUrl, credentialHandler);
 
+    // Get a reference to the Release API
+    const releaseApi = await connection.getReleaseApi();
+
+    // Get the release definition by name
+    const releaseDefinitions = await releaseApi.getReleaseDefinitions(projectName, releaseDefinitionName);
+    if (releaseDefinitions.length === 0) {
+      console.error(`Release definition not found with name: ${releaseDefinitionName}`);
+      process.exit(1);
+    }
+
+    // Use the first release definition (assuming there is only one release definition with the given name)
+    const releaseDefinition = releaseDefinitions[0];
+
     // Get a reference to the Build API
-    let build = await connection.getBuildApi();
+    const build = await connection.getBuildApi();
 
     if (!fs.existsSync(artifactPath)) {
       console.error(`Artifact path not found: ${artifactPath}`);
@@ -36,12 +51,9 @@ const run = async () => {
     };
     await build.createArtifact(projectName, artifactMetadata, artifactStream);
 
-    // Get a reference to the Release API
-    const releaseApi = await connection.getReleaseApi();
-
     // Create a new release
     const release = {
-      definitionId: 12,
+      definitionId: releaseDefinition.id,
       description: 'New release created by Azure DevOps pipeline',
       artifacts: [
         {
